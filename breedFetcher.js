93fac713d86e2451043fe1275d349b906232318c
Serah Sirohi <serahsirohi@gmail.com>
2020-07-31 00:15:47 +0000

    adding updated breedFetcher file, complete with eslint check

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,34 @@
 const request = require('request');
 const breed = process.argv[2];
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log(typeof body);
-
-  
-  
-  // //if requested breed is not found, then print out appropriate message
-  
-  if (error !== null) {
-    console.log('Error:', error.message);
-    process.exit();
-  }
-  
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Requested breed not found!');
-  } else {
-    //accessing the first element in the array and printing out the description for the user
-    // console.log('first element description', data[0]);
+const fetchBreedDescription = (err, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+    //error should be kept in it's own section - usually on top
+    if (error) callback(error,null);
+      // console.error('error:', error); // Print the error if one occurred
     
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    // console.log(typeof body);
+
+    const data = JSON.parse(body);
+    console.log(data); //makes the output look organised.
     const firstElement = data[0];
-    console.log('description: ', firstElement.description);
-       
-    // console.log('data', data);
-    // console.log('typeof:', typeof data);
-  }
-});
 
+    // EXPANDED FORM FOR LINE 30-31
+    // if (!firstElement) {
+    //   return callback('Requested breed not found!',null)
+    //   // console.log('Requested breed not found!');
+    // } else {
+    //   return callback(null,firstElement.description) 
+    //   // console.log('description: ', firstElement.description);
 
+    // }
+    //TERNARY OPERATOR for lines 20-24
+    !firstElement ? callback('Requested breed not found!',null) : callback(null,firstElement.description); 
 
+  });
+}
+
+module.exports = { fetchBreedDescription };
 
